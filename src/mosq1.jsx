6c938b23b29c5092cffb730f8ee630a6c5b99c7e
Adnan Kuthradan <adnangafoor2002@gmail.com>
2023-09-13 21:14:48 +0530

    sidebar on new sub pages

--- src/mosq1.jsx
+++ src/mosq1.jsx
@@ -1,19 +1,31 @@
 import React from 'react';
+import Sidebar from './Sidebar'
 
 const MosquitoPage = () => {
   const mosquitoPageStyle = {
     backgroundColor: 'black',
-    
     width: '100vw', // 100% of the viewport width
     height: '100vh', // 100% of the viewport height
+    display: 'flex',
+    alignItems: 'center', // Center content vertically
   };
 
-  return <div style={mosquitoPageStyle}>
-    <h1  style={{color:'white'}}><center>mosquito page</center></h1>
-  </div>;
+  const textContainerStyle = {
+    flex: '1', // Allow the text container to take remaining space
+    padding: '20px', // Add some padding for spacing
+    color: 'white',
+  };
+
+  return (
+    <div style={mosquitoPageStyle}>
+      <Sidebar />
+      <div style={textContainerStyle}>
+        <h1>Mosquito page</h1>
+      </div>
+    </div>
+  );
 };
 
-
-
-
 export default MosquitoPage;
+
+
--- src/water1.jsx
+++ src/water1.jsx
@@ -1,16 +1,31 @@
 import React from 'react';
 
+import Sidebar from './Sidebar'
+
 const WaterPage = () => {
   const waterPageStyle = {
     backgroundColor: 'black',
-    
     width: '100vw', // 100% of the viewport width
     height: '100vh', // 100% of the viewport height
+    display: 'flex',
+    alignItems: 'center', // Center content vertically
   };
 
-  return <div style={waterPageStyle}>
-    <h1  style={{color:'white'}}><center>water page</center></h1>
-  </div>;
-};
 
+  const textContainerStyle = {
+    flex: '1', // Allow the text container to take remaining space
+    padding: '20px', // Add some padding for spacing
+    color: 'white',
+  };
+
+
+  return (
+    <div style={waterPageStyle}>
+     <Sidebar />
+      <div style={textContainerStyle}>
+        <h1>Water Page</h1>
+        </div>
+  </div>
+  );
+};
 export default WaterPage;
